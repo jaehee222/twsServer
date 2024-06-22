9a40007a8ca9bfc845d245752063a965ecd474a2
jaehee222 <j81719@naver.com>
2024-06-22 23:12:40 +0900

    new project

--- src/main/java/com/example/twsServer/controller/UserController.java
+++ src/main/java/com/example/twsServer/controller/UserController.java
@@ -1,2 +1,16 @@
-package com.example.twsServer.controller;public class userController {
+package com.example.twsServer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/twsServer")
+public class UserController {
+
+    // 리엑트에서 spring 호출가능한지 테스트
+    @GetMapping("/sample")
+    public String getSampleMessage(){
+        return "im spring";
+    }
 }
---
+++ src/test/java/com/example/twsServer/TwsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twsServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
