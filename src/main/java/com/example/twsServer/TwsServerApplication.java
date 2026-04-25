c845daca4e40474946d9958e6758cf96dd402bd6
jaehee222 <j81719@naver.com>
2026-04-25 23:14:11 +0900

    서버 확인용으로 임시 적용

--- src/main/java/com/example/twsServer/TwsServerApplication.java
+++ src/main/java/com/example/twsServer/TwsServerApplication.java
@@ -3,10 +3,24 @@ package com.example.twsServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// 임시 추가 (서버 확인용)
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
 public class TwsServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TwsServerApplication.class, args);
 	}
 }
+
+//@SpringBootApplication
+//public class TwsServerApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(TwsServerApplication.class, args);
+//	}
+//}
